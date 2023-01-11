335e3b439265aefbdb8bca21a6a1877c6171c1b4
dreamik <dream-ik89@naver.com>
2023-01-11 20:27:32 +0900

    feat(userJoin):회원가입 후 DB처리
    회원가입 DB처리, 그후 3초 뒤 loginView로 전환
    
    공용API로 주소 입력, 비밀번호 확인 js처리 필요

--- ycHackers/src/main/java/com/ych/pjt/dao/UserDao.java
+++ ycHackers/src/main/java/com/ych/pjt/dao/UserDao.java
@@ -3,7 +3,28 @@ package com.ych.pjt.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ych.pjt.dto.UserDataDto;
+
 public class UserDao implements IUserDao {
 	@Autowired
 	private SqlSession sqlSession;
+
+	@Override
+	public String userJoin(UserDataDto dto) {
+		System.out.println("userJoin method");
+		String result = null;
+		try {
+			int res = sqlSession.insert("userJoin",dto);
+			System.out.println("dao res: "+res);
+			if(res>0) {
+				result="success";
+			}else {
+				result="failed";
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			result="failed";
+		}
+		return result;
+	}
 }
