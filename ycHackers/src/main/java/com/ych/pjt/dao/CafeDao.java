56cb79da4e0ec839722213c823448a0c8b2bae90
ojh <twzt9983@naver.com>
2023-01-19 20:55:08 +0900

    feat : cafeList
    main - cafeList ajax 연결
    cafeList db 연동

--- ycHackers/src/main/java/com/ych/pjt/dao/CafeDao.java
+++ ycHackers/src/main/java/com/ych/pjt/dao/CafeDao.java
@@ -1,5 +1,19 @@
 package com.ych.pjt.dao;
 
-public class CafeDao implements ICafeDao {
+import java.util.ArrayList;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ych.pjt.dto.CafeDto;
+
+public class CafeDao implements ICafeDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	public ArrayList<CafeDto> cafeList() {
+		ArrayList<CafeDto> dtos = (ArrayList)sqlSession.selectList("cafeList");
+		return dtos;
+	}
 }
